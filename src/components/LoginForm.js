1db52367757eccf469c15906c1f227f7a187bfe8
shelilevine <shelisteinberg@gmail.com>
2020-07-13 17:16:10 -0400

    fix: handle google login failure

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -8,26 +8,12 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Alert from "@material-ui/lab/Alert";
 import { oauthKey } from "../secrets";
-
-// function Copyright() {
-//   return (
-//     <Typography variant="body2" color="textSecondary" align="center">
-//       {"Copyright © "}
-//       <Link color="inherit" href="https://material-ui.com/">
-//         Serv'd
-//       </Link>{" "}
-//       {new Date().getFullYear()}
-//       {"."}
-//     </Typography>
-//   );
-// }
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -115,16 +101,13 @@ export default function SignIn(props) {
           </Button>
           <Grid container>
             <Grid item xs>
-              {/* <Link href="#" variant="body2">
-                Forgot password?
-              </Link> */}
               <GoogleLogin
                 clientId={oauthKey}
                 buttonText="Login"
                 // uxMode="redirect"
                 // redirectUri='http://localhost:3000/'
                 onSuccess={props.loginWithGoogle}
-                onFailure={props.loginWithGoogle}
+                // onFailure={props.loginWithGoogle}
                 cookiePolicy="single_host_origin"
                 SameSite="None"
               />
@@ -137,9 +120,6 @@ export default function SignIn(props) {
           </Grid>
         </form>
       </div>
-      {/* <Box mt={8}>
-        <Copyright />
-      </Box> */}
     </Container>
   );
 }
