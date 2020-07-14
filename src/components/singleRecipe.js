c63ae4b138ea658c31f17a545e6d21fc8b0f9061
Raghdaa <15679508+rab09011@users.noreply.github.com>
2020-07-14 10:38:42 -0400

    Update singleRecipe.js
    fixed imports

--- src/components/singleRecipe.js
+++ src/components/singleRecipe.js
@@ -1,18 +1,9 @@
 import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
+import { CssBaseline, Typography, Container, Link, Grid, Button, Card, CardActions, CardContent, Divider } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
 import { withRouter } from "react-router-dom";
-import Button from "@material-ui/core/Button";
 import StarBorderIcon from "@material-ui/icons/StarBorder";
 import StarIcon from "@material-ui/icons/Star";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Divider from "@material-ui/core/Divider";
 import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
 import PrintIcon from "@material-ui/icons/Print";
 import RateReviewIcon from "@material-ui/icons/RateReview";
@@ -113,7 +104,6 @@ export function SingleRecipe(props) {
       .length > 0;
 
   recipe.steps = recipe.steps || [];
-  console.log(recipe);
   return (
     <div className={classes.root}>
       <CssBaseline />
