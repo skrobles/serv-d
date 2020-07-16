f9a522cf008a7769a7755cacceedd531427ab4ae
Sara Kowalski <sarakowalski08@gmail.com>
2020-07-16 11:13:09 -0400

    feat: updated my account view

--- src/components/ViewAccountForm.js
+++ src/components/ViewAccountForm.js
@@ -1,76 +1,112 @@
 import React from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/styles';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { withStyles } from '@material-ui/core/styles';
 import {
-  Card,
-  CardHeader,
-  CardContent,
-  CardActions,
-  Divider,
-  Grid,
+  Container,
+  Avatar,
+  CssBaseline,
+  Typography,
   Button,
   TextField,
+  makeStyles,
 } from '@material-ui/core';
 
-const useStyles = makeStyles(() => ({
-  root: {
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    backgroundColor: 'white',
+    padding: '30px',
+    borderRadius: '10px',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
   },
 }));
 
 export default function ViewAccountForm(props) {
   const classes = useStyles();
   console.log('account form props', props);
-  const { className, ...rest } = props;
+  // const { className, ...rest } = props;
   const { onChange, onSubmit } = props;
-  const { name, email } = props.state;
+  const { name, email, password } = props.state;
 
   return (
-    <Card {...rest} className={clsx(classes.root, className)}>
-      <form autoComplete="off" noValidate onSubmit={(evt) => onSubmit(evt)}>
-        <CardHeader
-          subheader="The information can be edited"
-          title="My Account"
-        />
-        <Divider />
-        <CardContent>
-          <Grid container spacing={3}>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                helperText="Please specify your name"
-                label="Name"
-                margin="dense"
-                name="name"
-                onChange={(evt) => onChange(evt)}
-                required
-                value={name}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label="Email Address"
-                margin="dense"
-                name="email"
-                onChange={(evt) => onChange(evt)}
-                required
-                value={email}
-                variant="outlined"
-              />
-            </Grid>
-          </Grid>
-        </CardContent>
-        <Divider />
-        <CardActions>
-          <Button color="primary" variant="contained" type="submit">
-            Save details
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <AccountCircleIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Edit Account Information
+        </Typography>
+        <form
+          className={classes.form}
+          noValidate
+          onSubmit={(evt) => onSubmit(evt)}
+        >
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="name"
+            label="name"
+            type="name"
+            id="name"
+            onChange={(evt) => onChange(evt)}
+            value={name}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={(evt) => onChange(evt)}
+            value={email}
+            type="email"
+            pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(evt) => onChange(evt)}
+            value={password}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Update Account
           </Button>
-        </CardActions>
-      </form>
-    </Card>
+        </form>
+      </div>
+    </Container>
   );
 }
