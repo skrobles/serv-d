cd5c6bc495ef663f3fbaae0862af084f1046ed36
shelilevine <shelisteinberg@gmail.com>
2020-08-13 11:16:27 -0400

    cleanup comments

--- src/components/Login.js
+++ src/components/Login.js
@@ -24,7 +24,7 @@ const oauthKey = process.env.REACT_APP_OAUTHKEY;
 
 const serverUrl = "/api/auth";
 
-const styles = {
+const useStyles = makeStyles((theme) => ({
   formContainer: {
     alignContent: "center",
     display: "flex",
@@ -33,71 +33,6 @@ const styles = {
     flexDirection: "column",
     minHeight: "100vh",
   },
-};
-
-// export class Login extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       email: "",
-//       password: "",
-//       error: null,
-//     };
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//     this.loginWithGoogle = this.loginWithGoogle.bind(this);
-//   }
-
-// handleChange(evt) {
-//   this.setState({ [evt.target.name]: evt.target.value, error: null });
-// }
-
-// async handleSubmit(evt) {
-//   evt.preventDefault();
-//   try {
-//     const { data } = await axios.post(`${serverUrl}/signin`, this.state);
-//     if (data.id) {
-//       this.props.setUser(data);
-//       this.props.history.push(this.props.history.location.state);
-//     } else {
-//       this.setState({ error: "Invalid username and/or password" });
-//     }
-//   } catch (err) {
-//     this.setState({ error: "Invalid username and/or password" });
-//     console.log(err);
-//   }
-// }
-
-// async loginWithGoogle(response) {
-//   const idToken = response.getAuthResponse().id_token;
-//   try {
-//     const { data } = await axios.post(`${serverUrl}/google`, {
-//       token: idToken,
-//     });
-//     this.props.setUser(data);
-//     this.props.history.push(this.props.history.location.state);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
-
-//   render() {
-//     return this.props.user.id ? (
-//       <Redirect to={this.props.history.location.state} />
-//     ) : (
-//       <Box mx="auto" style={styles.formContainer}>
-//         {/* <LoginForm
-//           onChange={this.handleChange}
-//           onSubmit={this.handleSubmit}
-//           state={this.state}
-//           loginWithGoogle={this.loginWithGoogle}
-//         /> */}
-//       </Box>
-//     );
-//   }
-// }
-
-const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
     display: "flex",
@@ -162,7 +97,7 @@ export function SignIn(props) {
   return props.user.id ? (
     <Redirect to={props.history.location.state} />
   ) : (
-    <Box mx="auto" style={styles.formContainer}>
+    <Box mx="auto" className={classes.formContainer}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <div className={classes.paper}>
@@ -188,7 +123,6 @@ export function SignIn(props) {
               name="email"
               autoComplete="email"
               autoFocus
-              // onChange={(evt) => onChange(evt)}
               onChange={(evt) => {
                 setError(null);
                 setEmail(evt.target.value);
@@ -207,7 +141,6 @@ export function SignIn(props) {
               type="password"
               id="password"
               autoComplete="current-password"
-              // onChange={(evt) => onChange(evt)}
               onChange={(evt) => {
                 setError(null);
                 setPw(evt.target.value);
@@ -253,5 +186,4 @@ export function SignIn(props) {
   );
 }
 
-// export default withRouter(Login);
 export default withRouter(SignIn);
