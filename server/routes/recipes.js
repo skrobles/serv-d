ed3cea3b32531bcbd08950031558f34a327afcfa
shelilevine <shelisteinberg@gmail.com>
2020-09-24 14:21:58 -0400

    refactor: updated error handling in recipe routes

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -7,57 +7,36 @@ module.exports = router.routes();
 
 //search recipes by ingredients
 router.get("/", async (ctx, next) => {
-  try {
-    const recipes = await getRecipes(ctx.query);
-    ctx.body = recipes;
-  } catch (err) {
-    console.log(err);
-    ctx.throw(500, "Could not get recipes");
-  }
+  if (!ctx.query || !ctx.query.ingredients)
+    ctx.throw(404, "Could not get recipes");
+  const recipes = await getRecipes(ctx.query);
+  ctx.body = recipes;
 });
 
 //save a recipe to a user account
 router.post("/", async (ctx, next) => {
   if (!ctx.session.user) ctx.throw(404, "Must be logged in to save recipe");
-  else
-    try {
-      const userId = ctx.session.user.uid;
-      const recipe = await saveRecipe(ctx.request.body, userId);
-      if (!recipe) ctx.throw(Error);
-      ctx.status = 201;
-    } catch (err) {
-      console.log(err);
-      ctx.throw(500, "Could not add recipe");
-    }
+  const userId = ctx.session.user.uid;
+  const recipe = await saveRecipe(ctx.request.body, userId);
+  if (!recipe) ctx.throw(500, "Could not add recipe");
+  ctx.status = 201;
 });
 
 //get a users saved recipes
 router.get("/saved", async (ctx, next) => {
   if (!ctx.session.user) ctx.throw(404, "Must be logged in for saved recipes");
-  else
-    try {
-      const recipes = [];
-      const recipeCollection = db.collection(ctx.session.user.uid);
-      const snapshot = await recipeCollection.get();
-      snapshot.forEach((doc) => {
-        recipes.push(doc.data());
-      });
-      ctx.body = recipes;
-    } catch (err) {
-      console.log(err);
-      ctx.throw(500, "Error getting saved recipes");
-    }
+  const recipes = [];
+  const recipeCollection = db.collection(ctx.session.user.uid);
+  const snapshot = await recipeCollection.get();
+  snapshot.forEach((doc) => {
+    recipes.push(doc.data());
+  });
+  ctx.body = recipes;
 });
 
 //remove a recipe from a user's account
 router.delete("/:title", async (ctx, next) => {
   if (!ctx.session.user) ctx.throw(404, "Must be logged in to remove a recipe");
-  else
-    try {
-      await db.collection(ctx.session.user.uid).doc(ctx.params.title).delete();
-      ctx.status = 200;
-    } catch (err) {
-      console.log(err);
-      ctx.throw(500, "Error removing recipe");
-    }
+  await db.collection(ctx.session.user.uid).doc(ctx.params.title).delete();
+  ctx.status = 200;
 });
